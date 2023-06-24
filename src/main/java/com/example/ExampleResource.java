729bf879a7ccdb16ce26768cc001ad28f1686d3c
fenix-hub <n.santilio97@gmail.com>
2023-06-24 11:31:07 +0200

    (changed) renamed endpoint methods

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,20 +1,13 @@
 package com.example;
 
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
-import java.awt.image.BufferedImage;
-import java.util.Base64;
 import java.util.Map;
-
-import static io.quarkus.devui.runtime.comms.MessageType.Response;
 
 @Path("")
 public class ExampleResource {
@@ -25,14 +18,14 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/qr")
-    public Map<String, String> getTokenQR3() throws Exception {
+    public Map<String, String> getQR() throws Exception {
         return service.getData();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/tokens")
-    public Multi<Service.Token> getTokens() {
-        return service.getTokens();
+    public Multi<String> getChannels() {
+        return service.getChannels();
     }
 }
