c26a7ce09a7f4f3359566081631f019e762e25ce
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-07-11 15:01:09 +0200

    update tests

--- src/main/java/com/example/Service.java
+++ src/main/java/com/example/Service.java
@@ -1,17 +1,11 @@
 package com.example;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
+import io.nayuki.qrcodegen.QrCode;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.awt.image.BufferedImage;
-import java.util.Base64;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @ApplicationScoped
@@ -20,11 +14,27 @@ public class Service {
     private final Queue<String> channelQueue = new ConcurrentLinkedQueue<String>();
     private final Random random = new Random();
 
-    private BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix =
-                barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    private BufferedImage toImage(QrCode qr, int scale, int border, int lightColor, int darkColor) {
+        Objects.requireNonNull(qr);
+        if (scale <= 0 || border < 0)
+            throw new IllegalArgumentException("Value out of range");
+        if (border > Integer.MAX_VALUE / 2 || qr.size + border * 2L > Integer.MAX_VALUE / scale)
+            throw new IllegalArgumentException("Scale or border too large");
+
+        BufferedImage result = new BufferedImage((qr.size + border * 2) * scale, (qr.size + border * 2) * scale, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < result.getHeight(); y++) {
+            for (int x = 0; x < result.getWidth(); x++) {
+                boolean color = qr.getModule(x / scale - border, y / scale - border);
+                result.setRGB(x, y, color ? darkColor : lightColor);
+            }
+        }
+        return result;
+    }
+
+
+    private BufferedImage generateQRCodeImage(String barcodeText) {
+        QrCode qr = QrCode.encodeBinary(barcodeText.getBytes(), QrCode.Ecc.MEDIUM);
+        return toImage(qr, 5, 2, 0xFFFFFF, 0x000000);
     }
 
     // Get QR code as byte array writing from BufferedImage
--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -3,19 +3,31 @@ package com.example;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
+    @SuppressWarnings("unchecked")
+    void testQREndpoint() {
+        HashMap<String, String> responseBody = (HashMap<String, String>) given()
+                .when().get("/qr")
                 .then()
                 .statusCode(200)
-                .body(is("Hello from RESTEasy Reactive"));
+                .contentType("application/json")
+                .extract()
+                .body().as(HashMap.class);
+    }
+
+    @Test
+    void testChannelsEndpoint() {
+        given()
+            .when().get("/tokens")
+            .then()
+            .statusCode(200);
     }
 
 }
